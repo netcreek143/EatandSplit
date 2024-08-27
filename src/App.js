1cd364a216d278c59d9da0b64fa76ec94a2dff20
Suriya <104789092+netcreek143@users.noreply.github.com>
2024-08-27 11:40:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ const initialFriends = [
   {
     id     : 118836,
     name   : "Bharath",
-    image  : "https://i.pravatar.cc/48?u=118836",
+    image  : "https://i.pravatar.cc/150?img=3",
     balance: -7
   }, {
     id     : 933372,
     name   : "Somi",
-    image  : "https://i.pravatar.cc/48?u=933372",
+    image  : "https://i.pravatar.cc/150?img=12",
     balance: 20
   }, {
     id     : 499476,
     name   : "Sakthi",
-    image  : "https://i.pravatar.cc/48?u=499476",
+    image  : "https://i.pravatar.cc/150?img=70",
     balance: 0
   }
 ];
