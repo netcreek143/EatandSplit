a901ef5c336b363f3372511f5835ff887aca53cd
Suriya <104789092+netcreek143@users.noreply.github.com>
2024-08-27 11:36:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,17 +8,17 @@ import SideBar from "./components/SideBar";
 const initialFriends = [
   {
     id     : 118836,
-    name   : "Clark",
+    name   : "Bharath",
     image  : "https://i.pravatar.cc/48?u=118836",
     balance: -7
   }, {
     id     : 933372,
-    name   : "Sarah",
+    name   : "Somi",
     image  : "https://i.pravatar.cc/48?u=933372",
     balance: 20
   }, {
     id     : 499476,
-    name   : "Anthony",
+    name   : "Sakthi",
     image  : "https://i.pravatar.cc/48?u=499476",
     balance: 0
   }
